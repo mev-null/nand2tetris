9d9ea9bc5accf4cc942e5c20fdfc7aa78a5d4800
mev-null <190558379+mev-null@users.noreply.github.com>
2026-09-24 23:55:19 +0900

    feat: push const/local

---
+++ projects/07-08-vm-translator/tests/code_writer_test.cpp
@@ -0,0 +1,55 @@
+#include "code_writer.hpp"
+
+#include <sstream>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "absl/status/status_matchers.h"
+#include "command.hpp"
+
+using ::absl_testing::IsOk;
+using ::testing::Not;
+
+namespace hack::vm {
+namespace {
+
+using ::absl_testing::IsOk;
+
+TEST(CodeWriterTest, WritesPushConstant) {
+  std::ostringstream output;
+  CodeWriter writer(output, "Foo");
+
+  EXPECT_THAT(writer.WriteCommand({CommandType::kPush, std::nullopt, Segment::kConstant, 7}),
+              IsOk());
+  EXPECT_EQ(output.str(),
+            "@7\n"
+            "D=A\n"
+            "@SP\n"
+            "A=M\n"
+            "M=D\n"
+            "@SP\n"
+            "M=M+1\n");
+}
+
+TEST(CodeWriterTest, WritesPushLocal) {
+  std::ostringstream output;
+  CodeWriter writer(output, "Foo");
+
+  EXPECT_THAT(writer.WriteCommand({CommandType::kPush, std::nullopt, Segment::kLocal, 7}), IsOk());
+  EXPECT_EQ(output.str(),
+            "@LCL\n"
+            "D=M\n"
+            "@7\n"
+            "D=D+A\n"
+            "A=D\n"
+            "D=M\n"
+            "@SP\n"
+            "A=M\n"
+            "M=D\n"
+            "@SP\n"
+            "M=M+1\n");
+}
+
+}  // namespace
+}  // namespace hack::vm
